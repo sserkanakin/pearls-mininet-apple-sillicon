a8f339544c8b9701026aa8c3b3cddc69ab8386fd
sserkanakin <115831261+sserkanakin@users.noreply.github.com>
2022-10-14 20:39:52 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,9 @@ QTR=21wi
 MININET_VERSION="2.3.1b1"
 POX_VERSION=fangtooth
 
+sudo apt-get --allow-releaseinfo-change update
 sudo apt-get update
-sudo apt-get install -y python2 python-is-python2 unzip net-tools
+sudo apt install git
 git clone https://github.com/mininet/mininet
 (cd mininet && git checkout $MININET_VERSION)
 sed -i 's/cgroup-bin/cgroup-tools/g' mininet/util/install.sh
